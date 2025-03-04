f278224060031e3624b1cd35866299614bb98d01
brandontPortillo <5834255@clases.edu.sv>
2025-03-03 20:13:06 -0600

    Primer commit

---
+++ CategoryCreateArraySessionVariables.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CategoryCreateArraySessionVariables.aspx.cs" Inherits="One_DimensionArray5834255.CategoryCreateArraySessionVariables" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Create Category Array</h1><br /><br />
+            <asp:Button runat="server" ID="btnCreateArray" OnClick="btnCreateArray_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ CategoryCreateArraySessionVariables.aspx.designer.cs
@@ -0,0 +1,35 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace One_DimensionArray5834255
+{
+
+
+    public partial class CategoryCreateArraySessionVariables
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control btnCreateArray.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCreateArray;
+    }
+}
